02f3443e7689771211c6efc254d75310869b7046
amkumar <amkumar@tripadvisor.com>
2019-09-11 14:20:26 +1000

    yo

--- src/com/mindtube/selenium/TestSel.java
+++ src/com/mindtube/selenium/TestSel.java
@@ -1,7 +1,20 @@
 package com.mindtube.selenium;
 
-public class Hello {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSel {
     public static void main(String[] args) {
-        System.out.println("jij");
+        System.setProperty("webdriver.chrome.driver","/Users/amkumar/dev/code/Amit-code/selenium/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+        driver.findElement(By.id("MainContent_txtUserName")).sendKeys("test@test.com");
+        driver.findElement(By.id("MainContent_txtPassword")).sendKeys("test");
+        driver.findElement(By.id("MainContent_btnLogin")).click();
+
+        String msg = driver.findElement(By.id("conf_message")).getText();
+        System.out.println(msg);
+        
     }
 }
